c80a3d2bc832347c3a1622453bc8a140e3090788
Babalang <langouetbastian@gmail.com>
2026-04-30 17:25:47 +0200

    improving mesh creation

--- src/microscopy_metrics/metricTool/meshTool.py
+++ src/microscopy_metrics/metricTool/meshTool.py
@@ -6,14 +6,15 @@ import open3d as o3d
 import pyvista as pv
 from skimage.segmentation import clear_border
 from scipy.ndimage import gaussian_filter
+from microscopy_metrics.thresholdTools.legacy import ThresholdLegacy
 
 class MeshBuilder(object):
     def __init__(self, image=None):
         self._image = image
         self._pixelSize = [1.0, 1.0, 1.0]
         self._RMSMaxError = 0.02
-        self._numIterations = 500
-        self._curvatureScaling = 0.1
+        self._numIterations = 100
+        self._curvatureScaling = 5.0
         self._vertices = None
         self._faces = None
         self._concavity = None
@@ -24,12 +25,18 @@ class MeshBuilder(object):
     def BuildMesh(self):
         if self._image is None : 
             raise ValueError("Image is not set. Please set the image before building the mesh.")
-        imageVolume = sitk.GetImageFromArray(self._image.astype(np.float32))
+        imgArray = self._image.astype(np.float32)
+        imgBlurred = gaussian_filter(imgArray, sigma=0.5)
+        maxImg = np.max(imgBlurred)
+        NormImg = imgBlurred / maxImg if maxImg > 0 else imgBlurred
+        enhancedImg = (NormImg ** 1.25) * maxImg
+        imageVolume = sitk.GetImageFromArray(enhancedImg)
 
         otsuFilter = sitk.TriangleThresholdImageFilter()
         otsuFilter.SetInsideValue(0)
         otsuFilter.SetOutsideValue(1)
-        binaryImage = sitk.Cast(otsuFilter.Execute(imageVolume), sitk.sitkFloat32)
+        thesholdImage = otsuFilter.Execute(imageVolume)
+        binaryImage = sitk.Cast(thesholdImage, sitk.sitkFloat32)
         binaryImage = binaryImage * (-2.0) + 1.0
 
         chanVese = sitk.ScalarChanAndVeseDenseLevelSetImageFilter()
@@ -37,26 +44,26 @@ class MeshBuilder(object):
         chanVese.SetMaximumRMSError(self._RMSMaxError)
         chanVese.SetCurvatureWeight(self._curvatureScaling)
         segmentation = chanVese.Execute(binaryImage, imageVolume)
+
         segArray = sitk.GetArrayFromImage(segmentation)
         binaryImage = clear_border(segArray.astype(bool))
+
         LabelledImage = measure.label(binaryImage)
         regions = measure.regionprops(LabelledImage)
         if len(regions) == 0:
             raise ValueError("No regions found in the image. Please check the input image.")
         largestRegion = max(regions, key=lambda r: r.area)
         self._largestRegionMask = (LabelledImage == largestRegion.label).astype(float)
-        self._largestRegionMask = gaussian_filter(self._largestRegionMask, sigma=1.0)
+        self._largestRegionMask = gaussian_filter(self._largestRegionMask, sigma=0.5)
 
         self._vertices, self._faces, _, _ = measure.marching_cubes(self._largestRegionMask, level=0.5)
 
         mesh = o3d.geometry.TriangleMesh()
         mesh.vertices = o3d.utility.Vector3dVector(self._vertices)
         mesh.triangles = o3d.utility.Vector3iVector(self._faces)
+        mesh = mesh.filter_smooth_simple(number_of_iterations=5)
         mesh.remove_duplicated_vertices()
         mesh.remove_duplicated_triangles()
-        mesh.remove_degenerate_triangles()
-        mesh.remove_non_manifold_edges()
-        mesh.remove_unreferenced_vertices()
         mesh.compute_vertex_normals()
         self._vertices = np.asarray(mesh.vertices) * np.array(self._pixelSize)
         self._faces = np.asarray(mesh.triangles)
@@ -65,15 +72,10 @@ class MeshBuilder(object):
     def concavity(self):
         if self._vertices is None or self._faces is None:
             raise ValueError("Mesh has not been built. Please build the mesh before calculating concavity.")
-        mesh = o3d.geometry.TriangleMesh()
-        mesh.vertices = o3d.utility.Vector3dVector(self._vertices)
-        mesh.triangles = o3d.utility.Vector3iVector(self._faces)
-        if mesh.is_watertight():
-            meshVolume = mesh.get_volume()
-        else:
-            print("Warning: Mesh is not watertight! Using Voxel Volume fallback.")
-            voxel_volume = self._pixelSize[0] * self._pixelSize[1] * self._pixelSize[2]
-            meshVolume = np.sum(self._largestRegionMask) * voxel_volume
+        p1 = self._vertices[self._faces[:, 0]]
+        p2 = self._vertices[self._faces[:, 1]]
+        p3 = self._vertices[self._faces[:, 2]]
+        meshVolume = abs(np.sum(p1 * np.cross(p2, p3, axis=1)) / 6.0)
         hull = ConvexHull(self._vertices)
         hullVolume = hull.volume
         print(f"Mesh Volume: {meshVolume}, Hull Volume: {hullVolume}")
